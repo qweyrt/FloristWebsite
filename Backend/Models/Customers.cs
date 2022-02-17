222ce3442818c2aff2659d66c28dbc7ee52e7b93
qweyrt <81255174+qweyrt@users.noreply.github.com>
2022-02-17 18:03:58 +0700

    Add files via upload

---
+++ Backend/Models/Customers.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace FloristWebsite.Models
+{
+    public partial class Customers
+    {
+        public Customers()
+        {
+            Bouquets = new HashSet<Bouquets>();
+        }
+
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        public string Gender { get; set; }
+        public string Address { get; set; }
+        public int? PhoneNumber { get; set; }
+
+        public virtual ICollection<Bouquets> Bouquets { get; set; }
+    }
+}
